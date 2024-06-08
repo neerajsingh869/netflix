c78163137f19944ff7f6693089e70a54b90bf3c1
Neeraj Singh <869neerajsingh@gmail.com>
2024-06-08 12:34:54 +0530

    feat: create basic components, pages, and setup initial routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Login from './pages/Login.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Browse from './pages/Browse.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />
+  },
+  {
+    path: "/browse",
+    element: <Browse />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
