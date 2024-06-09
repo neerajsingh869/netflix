2dcad99425ab7e960a0bc7d691a1e57c5486aa72
Neeraj Singh <869neerajsingh@gmail.com>
2024-06-09 19:07:32 +0530

    feat: add store on app level

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ import Login from './pages/Login.jsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Browse from './pages/Browse.jsx'
 import { Toaster } from 'react-hot-toast';
-import { app } from './configs/firebase.js';
+import App from './App.jsx';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
 
 const router = createBrowserRouter([
   {
@@ -20,7 +22,10 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    <Toaster />
+    <Provider store={store}>
+      <App />
+      <RouterProvider router={router} />
+      <Toaster />
+    </Provider>
   </React.StrictMode>,
 )
