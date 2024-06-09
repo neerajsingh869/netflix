530df771388790b88d721b8dc32d15a968496a24
Neeraj Singh <869neerajsingh@gmail.com>
2024-06-09 19:05:20 +0530

    feat: use onAuthChanged firebase api to listen to auth state changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { useEffect } from 'react'
 import './App.css'
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './configs/firebase';
+import {useDispatch} from "react-redux";
+import { addUser, removeUser } from './redux/slices/userSlice';
 
 function App() {
-  return (
-    <h1 className="text-red-900 text-5xl font-bold">
-      Hello world!
-    </h1>
-  )
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const {uid, email, displayName} = user;
+        console.log(user);
+        dispatch(addUser({uid, email, displayName}));
+        // ...
+      } else {
+        // User is signed out
+        // ...
+        dispatch(removeUser());
+      }
+    });
+  }, [dispatch]);
 }
 
 export default App
