d3a0e3227a2b74ff37fc162435a9d18de5f0ccb5
Neeraj Singh <869neerajsingh@gmail.com>
2024-06-17 03:11:50 +0530

    feat: move logic of updating trailer and getting trailer info to primary container

--- src/components/EntryPageTrailer.jsx
+++ src/components/EntryPageTrailer.jsx
@@ -1,50 +1,9 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux"
-import { API_OPTIONS } from "../utils/constants";
-import { addTrailer } from "../redux/slices/movieSlice";
+/* eslint-disable react/prop-types */
 
-const EntryPageTrailer = () => {
-  const {nowPlayingMovies, trailer} = useSelector(store => store.movies);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (nowPlayingMovies.length > 0) {
-      const fetchMoviePickedTrailer = async () => {
-        const moviePicked = nowPlayingMovies[0];
-        const movieId = moviePicked.id;
-
-        try {
-          let response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`, API_OPTIONS);
-
-          response = await response.json();
-          console.log(response.results);
-
-          const filteredTrailers = response.results.filter(video => video.type === "Trailer");
-
-          let mainTrailer;
-          if (filteredTrailers && filteredTrailers.length > 0) {
-            mainTrailer = filteredTrailers.filter(trailer => trailer.name === "Official Trailer")[0];
-          } else {
-            mainTrailer = filteredTrailers[0];
-          }
-
-          console.log(mainTrailer);
-          dispatch(addTrailer(mainTrailer));
-
-        } catch (error) {
-          console.log(error.message);
-        }
-      }
-  
-      fetchMoviePickedTrailer();
-    }
-  }, [nowPlayingMovies, dispatch])
-
-  if (nowPlayingMovies.length === 0 || !trailer) return null;
-
+const EntryPageTrailer = ({trailer}) => {
   return (
     <div className="w-screen h-screen overflow-hidden relative">
-      <iframe className="w-screen absolute -top-24" style={{height: "calc(100vh + 200px)"}} src={`https://www.youtube.com/embed/${trailer.key}?rel=0&controls=0&autoplay=1&mute=1&loop=1`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe className="w-screen absolute -top-24" style={{height: "calc(100vh + 200px)"}} src={`https://www.youtube.com/embed/${trailer.key}?rel=0&controls=0&autoplay=1&mute=1&loop=1`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
     </div>
   )
 }
--- src/components/PrimaryContainer.jsx
+++ src/components/PrimaryContainer.jsx
@@ -1,10 +1,52 @@
+import { useDispatch, useSelector } from "react-redux";
 import EntryPageTrailer from "./EntryPageTrailer"
 import EntryPageTrailerDescription from "./EntryPageTrailerDescription"
+import { useEffect } from "react";
+import { addTrailer } from "../redux/slices/movieSlice";
+import { API_OPTIONS } from "../utils/constants";
 
 const PrimaryContainer = () => {
+  const {nowPlayingMovies, trailer} = useSelector(store => store.movies);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (nowPlayingMovies.length > 0) {
+      const fetchMoviePickedTrailer = async () => {
+        const moviePicked = nowPlayingMovies[0];
+        const movieId = moviePicked.id;
+
+        try {
+          let response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`, API_OPTIONS);
+
+          response = await response.json();
+          console.log(response.results);
+
+          const filteredTrailers = response.results.filter(video => video.type === "Trailer");
+
+          let mainTrailer;
+          if (filteredTrailers && filteredTrailers.length > 0) {
+            mainTrailer = filteredTrailers.filter(trailer => trailer.name === "Official Trailer")[0];
+          } else {
+            mainTrailer = filteredTrailers[0];
+          }
+
+          console.log(mainTrailer);
+          dispatch(addTrailer(mainTrailer));
+
+        } catch (error) {
+          console.log(error.message);
+        }
+      }
+  
+      fetchMoviePickedTrailer();
+    }
+  }, [nowPlayingMovies, dispatch])
+
+  if (nowPlayingMovies.length === 0 || !trailer) return null;
+
   return (
     <div>
-      <EntryPageTrailer />
+      <EntryPageTrailer trailer={trailer} />
       <EntryPageTrailerDescription /> 
     </div>
   )
