2312f4b2b1fc5c7f94860e311208ff97ca503bbb
Neeraj Singh <869neerajsingh@gmail.com>
2024-06-17 03:16:59 +0530

    feat: create custom hook to dispatch action to add trailer in movies store

---
+++ src/hooks/useEntryPageTrailer.jsx
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addTrailer } from "../redux/slices/movieSlice";
+import { API_OPTIONS } from "../utils/constants";
+
+const useEntryPageTrailer = (nowPlayingMovies) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (nowPlayingMovies.length > 0) {
+      const fetchMoviePickedTrailer = async () => {
+        const moviePicked = nowPlayingMovies[0];
+        const movieId = moviePicked.id;
+
+        try {
+          let response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`, API_OPTIONS);
+
+          response = await response.json();
+          console.log(response.results);
+
+          const filteredTrailers = response.results.filter(video => video.type === "Trailer");
+
+          let mainTrailer;
+          if (filteredTrailers && filteredTrailers.length > 0) {
+            mainTrailer = filteredTrailers.filter(trailer => trailer.name === "Official Trailer")[0];
+          } else {
+            mainTrailer = filteredTrailers[0];
+          }
+
+          console.log(mainTrailer);
+          dispatch(addTrailer(mainTrailer));
+
+        } catch (error) {
+          console.log(error.message);
+        }
+      }
+  
+      fetchMoviePickedTrailer();
+    }
+  }, [nowPlayingMovies, dispatch])
+}
+
+export default useEntryPageTrailer;
