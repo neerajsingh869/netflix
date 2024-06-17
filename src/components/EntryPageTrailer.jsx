38ebcd2d99107053d093f367544bd82e02136965
Neeraj Singh <869neerajsingh@gmail.com>
2024-06-17 14:37:34 +0530

    refactor: update trailer iframe

--- src/components/EntryPageTrailer.jsx
+++ src/components/EntryPageTrailer.jsx
@@ -3,7 +3,7 @@
 const EntryPageTrailer = ({trailer}) => {
   return (
     <div className="w-screen h-screen overflow-hidden relative">
-      <iframe className="w-screen absolute -top-24" style={{height: "calc(100vh + 200px)"}} src={`https://www.youtube.com/embed/${trailer.key}?rel=0&controls=0&autoplay=1&mute=1&loop=1`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+      <iframe className="w-screen absolute -top-32" style={{height: "calc(100vh + 200px)"}} src={`https://www.youtube.com/embed/${trailer.key}?rel=0&controls=0&autoplay=1&mute=1&loop=1`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
     </div>
   )
 }
