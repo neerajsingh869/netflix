47ede416adf5613cac9a8f5ad87636b1d4fa7b1f
Neeraj Singh <869neerajsingh@gmail.com>
2024-06-22 15:48:34 +0530

    style: make entry page trailer look amazing

--- src/components/EntryPageTrailer.jsx
+++ src/components/EntryPageTrailer.jsx
@@ -2,8 +2,8 @@
 
 const EntryPageTrailer = ({trailer}) => {
   return (
-    <div className="w-screen h-screen overflow-hidden relative">
-      <iframe className="w-screen absolute -top-32" style={{height: "calc(100vh + 200px)"}} src={`https://www.youtube.com/embed/${trailer.key}?rel=0&controls=0&autoplay=1&mute=1&loop=1`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+    <div className="w-screen overflow-hidden relative" style={{height: "92vh"}}>
+      <iframe className="w-screen aspect-video absolute -top-28" src={`https://www.youtube.com/embed/${trailer.key}?rel=0&controls=0&autoplay=1&mute=1&loop=1`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
     </div>
   )
 }
