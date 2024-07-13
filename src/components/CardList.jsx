982aac4f757dfb8739736987c1a8d5605963f9b9
Neeraj Singh <869neerajsingh@gmail.com>
2024-07-13 19:16:46 +0530

    style: add transition and hover effect for explore all button

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -30,7 +30,15 @@ const responsive = {
 const CardList = ({ title, data }) => {
   return (
     <div className="pl-12">
-      <h2 className="pt-12 pb-4 text-base sm:text-xl font-bold text-white">{title}</h2>
+      <h2 className="pt-12 pb-4 text-base sm:text-xl font-bold text-white">
+        <div className="inline-block cursor-pointer group/title">
+          <div className="inline-block mr-3">{title}</div>
+          <div className="text-xs text-blue-400 inline-block align-middle ">
+            <div className="inline-block mr-1 transition duration-500 opacity-0 -translate-x-20 group-hover/title:opacity-100 group-hover/title:translate-x-0">Explore All</div>
+            <div className="text-sm inline-block before:content-['>'] transition duration-300 opacity-0 -translate-x-20 group-hover/title:opacity-100 group-hover/title:translate-x-0"></div>
+          </div>
+        </div>
+      </h2>
       <Carousel
         responsive={responsive}
         swipeable={true}
